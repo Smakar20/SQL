20dc0df4baac9b462f94d544097fb6cf82c6e40c
Sohini Makar <sohini.makar@gmail.com>
2018-02-08 12:05:31 -0800

    top three salaries in each of the department
    query to find employees who earn the top three salaries in each of the department

---
+++ top3SalaryInEachDept.sql
@@ -0,0 +1,41 @@
+/*The Employee table holds all employees. Every employee has an Id, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who earn the top three salaries in each of the department. For the above tables, your SQL 
+query should return the following rows.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+*/
+
+select d.name department, e.name employee, e.salary 
+from employee e,employee e2,department d 
+where e.departmentid = d.id and e.departmentid = e2.departmentid and 
+  e.Salary <= e2.Salary 
+group by d.id, e.name 
+having (count(distinct e2.salary)) <= 3
+order by d.name asc, e.salary desc;
