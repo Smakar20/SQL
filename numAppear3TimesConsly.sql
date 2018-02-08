8888656474eba2ac9686bebe8cd60e90d830c43d
Sohini Makar <sohini.makar@gmail.com>
2018-02-08 11:36:43 -0800

    all nums, appear at least three times consecutively

---
+++ numAppear3TimesConsly.sql
@@ -0,0 +1,27 @@
+/*Write a SQL query to find all numbers that appear at least three times consecutively.
+
++----+-----+
+| Id | Num |
++----+-----+
+| 1  |  1  |
+| 2  |  1  |
+| 3  |  1  |
+| 4  |  2  |
+| 5  |  1  |
+| 6  |  2  |
+| 7  |  2  |
++----+-----+
+For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
+
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+*/
+
+select distinct num as consecutivenums
+from logs l
+where 
+num = (select num from logs where id = l.id - 1) 
+and num = (select num from logs where id = l.id + 1) order by id;
